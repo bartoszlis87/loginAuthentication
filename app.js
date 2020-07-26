4b317855f6ed5b7929e46d14f33d5a93f38a852c
Bartosz Lis <bartoszlis87@gmail.com>
2020-07-26 13:49:48 +0200

    Easy password and fields checker

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ moongose.connect(db, { useNewUrlParser: true })
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//Bodyparser
+app.use(express.urlencoded({
+    extended: false
+}));
+
+
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,38 @@ router.get('/login', (req, res) => res.render('login'));
 //Register
 router.get('/register', (req, res) => res.render('register'));
 
+//register Handle
+router.post('/register', (req, res) => {
+    // console.log(req.body);
+    // res.send('Hello');
+    const { name, email, password, password2 } = req.body;
+
+//Check fields
+if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please check all fields' });
+  }
+//Check passwords
+if (password !== password2) {
+    errors.push({msg: 'Password wrong'})
+}
+// Password length
+if(password.length < 6 ) {
+    errors.push({msq: 'Password to short'});
+}
+
+if (errors.length > 0) {
+    res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+    });
+} else {
+    res.send('pass');
+}
+
+
+});
+
 module.exports = router;
